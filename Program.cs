46977c57944a2ec86318db84a16d4d739a1d6049
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-01-11 09:28:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsporedbaStringova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst1 = "Ovo je neki tekst.";
+            string tekst2 = "Ovo je NEKI tekst.";
+            string tekst3 = "ovo je neki tekst.";
+            string tekst4 = "Ovo je neki tekst.";
+
+            // različiti stringovi
+            Console.WriteLine(
+                string.Compare(tekst1, tekst2));
+
+            Console.WriteLine(
+                string.Compare(tekst1, tekst3));
+
+            //jednaki stringovi
+            Console.WriteLine(
+                string.Compare(tekst1, tekst4));
+
+            if(string.Compare(tekst1, tekst2) ==0)
+            {
+                Console.WriteLine("Stringovi su jednaki");  
+            }
+            else
+            {
+                Console.WriteLine("Stringovi nisu jednaki");    
+            }
+
+            if (string.Compare(tekst1, tekst3) == 0)
+            {
+                Console.WriteLine("Stringovi su jednaki");
+            }
+            else
+            {
+                Console.WriteLine("Stringovi nisu jednaki");
+            }
+
+            if (string.Compare(tekst1, tekst4) == 0)
+            {
+                Console.WriteLine("Stringovi su jednaki");
+            }
+            else
+            {
+                Console.WriteLine("Stringovi nisu jednaki");
+            }
+
+            Console.ReadKey();  
+        }       
+
+    }
+}
